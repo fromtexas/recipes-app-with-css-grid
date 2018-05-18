c08240c1fc9580f75ac04752fbef44310ed95a5b
fromtexas <telures@yandex.ru>
2018-05-18 20:52:30 +0300

    search

--- app/index.js
+++ app/index.js
@@ -1,1 +1,6 @@
 import '../assets/styles/index.scss';
+
+import Search from './models/Search';
+
+const search = new Search('pizza');
+search.getResults();
---
+++ app/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor (querry) {
+        this.querry = querry
+    }
+
+    async getResults () {
+        const key = '5a3172e0314ec52bc81582ccf001ef26';
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        try{
+            const result = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.querry}`);
+            this.result =  result.data.recipes;
+            console.log(this.result);
+        } catch (error){
+            console.log(error);  
+        } 
+    }
+}
