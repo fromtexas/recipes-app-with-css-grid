ec34bd97d465e54a3a7be9367c56f43e91769241
fromtexas <telures@yandex.ru>
2018-05-20 20:02:37 +0300

    preloader

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import '../assets/styles/index.scss';
 
-import {elements} from './views/base';
+import {elements, renderLoader, clearLoader} from './views/base';
 import * as searchView from './views/searchView';
 
 import Search from './models/Search';
@@ -16,14 +16,15 @@ const controlSearch = async () => {
 
         //2. new search obj and add it to state
         state.search = new Search (query);
-
         //3. prepare ui for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         //4. search for recepis
         await state.search.getResults();
 
+        clearLoader();
         //5. render results on ui
         searchView.renderResults(state.search.result);
 
